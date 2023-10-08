5f43562aa48f477f1a458d315b5ee53356b2613a
sapphire <chidinmaakunwa@gmail.com>
2023-10-08 14:09:25 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Sync the model with the database
+const seq = require("./config/db");
+
+seq.sequelize.sync();
+
+
+app.use(passport.initialize()); 
+require('./middleware/authEmail')(passport); 
+
+// Routes
+require('./routes/userRoutes')(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
